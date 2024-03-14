244f223dd59c3f31597a2c0fb4e634d8cb3e0a93
Jagger.H <jaggerwinner@gmail.com>
2024-03-15 01:24:35 +0800

    双trigger可运行！！

--- function_app.py
+++ function_app.py
@@ -1,6 +1,14 @@
+import logging
 import azure.functions as func
 
-# from WrapperFunction import app as fastapi_app
 from app.main import app as fastapi_app
 
 app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.schedule(schedule="*/20 */1 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
